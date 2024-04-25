cd87a5c55f989695a0f46b16673859586ff4d57d
mk <minkhent.dev@gmail.com>
2024-04-25 21:21:20 +0630

    feat:add GPU inference

--- annotate.py
+++ annotate.py
@@ -8,17 +8,22 @@ from tqdm import tqdm
 from model import load_model
 from utils import remove_duplicate_boxes, save_prediction
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model, processor = load_model()
+device = torch.device("cuda:0" if torch.cuda.is_available() 
+                      else "cpu")
+model, processor = load_model(device)
 
 
-def inference_batch(image, query_images):
+def inference_batch(image, query_images , device):
 
     inputs = processor(images=image, query_images=query_images, return_tensors="pt").to(
         device
     )
-    with torch.no_grad():
-        batch_outputs = model.image_guided_detection(**inputs)
+    if device == "cuda":
+        with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
+            batch_outputs = model.image_guided_detection(**inputs)
+    else:
+        with torch.no_grad():
+            batch_outputs = model.image_guided_detection(**inputs)
 
     target_sizes = torch.Tensor([image.size[::-1]] * len(query_images)).to(device)
     results = processor.post_process_image_guided_detection(
@@ -51,7 +56,7 @@ def main():
     with tqdm(total=len(raw_images)) as pbar:
         for raw_image in raw_images:
             raw_image = Image.fromarray(np.uint8(Image.open(raw_image))).convert("RGB")
-            pred_results = inference_batch(raw_image, query_images)
+            pred_results = inference_batch(raw_image, query_images , device)
 
             unique_boxes = remove_duplicate_boxes(pred_results)
             if len(unique_boxes):
